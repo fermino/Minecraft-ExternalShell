3a1eaf898a163b46c5a47f5ab3549ba29255bcd3
Fermín Olaiz <ferminolaiz@gmail.com>
2024-02-07 21:08:56 -0300

    Add gradle specs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
